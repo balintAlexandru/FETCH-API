cc264379a26ac734c6a97150805f5911808c482e
Balint Alexandru <balintalex737@gmail.com>
2022-02-09 17:31:33 +0200

    add files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,26 +18,30 @@ const showProducts = () => {
     productsList()
         .then(data => {
             data.forEach(product => {
-                const card = document.createElement('div');
-                const title = document.createElement('div');
-                const content = document.createElement('p');
-                const price = document.createElement('span');
-                const image = document.createElement('img');
-                image.setAttribute('src',`${product.image}`);
-                card.classList.add('product');
-                title.classList.add('title');
-                price.classList.add('price');
-                listProducts.appendChild(card);
-                title.innerHTML = `${product.name}`;
-                content.innerHTML = `${product.description}`;
-                price.innerHTML = `Price: ${product.price}$`;
-                card.appendChild(image);
-                card.appendChild(title);
-                card.appendChild(content);
-                card.appendChild(price);
+                Model(product);
             })})
         .catch(err => {
           alert(err.message);
     });
+}
+
+const Model = (product) => {
+    const card = document.createElement('div');
+    const title = document.createElement('div');
+    const content = document.createElement('p');
+    const price = document.createElement('span');
+    const image = document.createElement('img');
+    image.setAttribute('src',`${product.image}`);
+    card.classList.add('product');
+    title.classList.add('title');
+    price.classList.add('price');
+    title.innerHTML = `${product.name}`;
+    content.innerHTML = `${product.description}`;
+    price.innerHTML = `Price: ${product.price}$`;
+    listProducts.appendChild(card);
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(content);
+    card.appendChild(price);
 }
 
